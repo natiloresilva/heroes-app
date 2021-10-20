d2b739855b55f012b53a173af98b186518cb3692
Natalia <natalia.pupushi@gmail.com>
2021-10-20 10:02:31 +0200

    Added searchScreen using query params
    *Also added heroScreen to show hero's description
    *Refactor app with useMemo hook

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { getHeroesByPublisher } from '../selectors/getHeroesByPublisher'
 import HeroCard from './HeroCard'
 
 export const HeroList = ({ publisher }) => {
 
-    const heroes = getHeroesByPublisher(publisher)
+    //const heroes = getHeroesByPublisher(publisher)
+    // En lugar de utilizar esta función utilizaremos un useMemo, para que de esta manera se vuelvan a obtener la lista de heroes solo si el publisher cambia
+    const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher])
 
     return (
         <div className='animate__animated animate__fadeIn'>
--- src/components/routers/DashboardRoutes.js
+++ src/components/routers/DashboardRoutes.js
@@ -3,6 +3,7 @@ import { Redirect, Route, Switch } from 'react-router'
 import { DcScreen } from '../dc/DcScreen'
 import { HeroScreen } from '../heroes/HeroScreen'
 import { MarvelScreen } from '../marvel/MarvelScreen'
+import { SearchScreen } from '../search/SearchScreen'
 import { Navbar } from '../ui/NavBar'
 
 export const DashboardRoutes = () => {
@@ -12,8 +13,9 @@ export const DashboardRoutes = () => {
             <div className='container mt-2'>
                 <Switch>
                     <Route exact path='/marvel' component={MarvelScreen} />
-                    <Route exact path='/heroe/:heroeId' component={HeroScreen} />
+                    <Route exact path='/hero/:heroeId' component={HeroScreen} />
                     <Route exact path='/dc' component={DcScreen} />
+                    <Route exact path='/search' component={SearchScreen} />
 
                     <Redirect to='/marvel' />
                 </Switch>
