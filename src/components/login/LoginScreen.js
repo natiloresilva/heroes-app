6ddd1b1792c8e58becc484605fbe840200fa90a8
Natalia <natalia.pupushi@gmail.com>
2021-10-21 12:06:58 +0200

    login-logout using authReducer & authContext

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/authContext'
+import { types } from '../../types/types'
 
 export const LoginScreen = ({ history }) => {
 
+    const { dispatch } = useContext(AuthContext)
+
     const handleLogin = () => {
+
+        dispatch({
+            type: types.login,
+            payload: {
+                name: 'Galileo'
+            }
+        })
+
         history.replace('/')
     }
 
